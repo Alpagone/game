b03a3b06bcb484b87d065ad16ced55869af88193
Alpagone <99545805+Alpagone@users.noreply.github.com>
2024-03-03 15:40:02 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const kartlar = [
+    {
+        id: 1,
+        gelir: 10,
+        maliyet: 5,
+        sure: 10
+    },
+    {
+        id: 2,
+        gelir: 20,
+        maliyet: 10,
+        sure: 20
+    },
+    {
+        id: 3,
+        gelir: 50,
+        maliyet: 25,
+        sure: 30
+    }
+];
+
+let para = 100; // Assuming you want to start with 100$
+
+// Rest of your JavaScript code goes here
